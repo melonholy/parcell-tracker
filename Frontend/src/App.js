ca607706ff5894883da3c45140a9624546134cb7
melonholy <36301981+melonholy@users.noreply.github.com>
2019-04-22 01:17:39 +0300

    ...

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import MainPage from "./Components/MainPage"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <MainPage />
+      <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+          </Switch>
+        </Router>
       </Provider>
     );
   }
